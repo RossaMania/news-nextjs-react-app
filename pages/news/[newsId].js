b897dca0498ecc5893fdedfd8a01c7e6415653c9
Ross Clettenberg <raclettenberg@gmail.com>
2023-07-30 20:06:53 -0500

    Make router.query.newsId into a newsId const.

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -6,7 +6,7 @@ function DetailsPage() {
 
   const router = useRouter();
 
-  router.query.newsId
+  const newsId = router.query.newsId
   console.log(router.query.newsId);
 
   return <h1>The Details Page!</h1>;
