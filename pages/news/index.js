eb2ea1a282086bbb7ce980983e661bed403ff65b
Ross Clettenberg <raclettenberg@gmail.com>
2023-07-30 20:54:20 -0500

    Create ul with three li's. Wrap in Fragment. Write three placeholder li elements. Import and add  Link tag to li's.

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,31 @@
 // our-domain.com/news
 
+import Link from "next/link";
+import { Fragment } from "react";
+
 function NewsPage() {
-  return <h1>The News Page!</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page!</h1>
+      <ul>
+        <li>
+          <Link href="/news/something">
+            Something!
+          </Link>
+        </li>
+        <li>
+          <Link href="/news/something-else">
+            Something else!
+          </Link>
+        </li>
+        <li>
+          <Link href="/news/something-else-again">
+            Something else again!
+          </Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
