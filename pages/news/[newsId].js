5643b5f3a8468dc39e0b7c4d85cfd12fe5616881
Ross Clettenberg <raclettenberg@gmail.com>
2023-07-30 20:03:03 -0500

    Import useRouter hook. Setup router const with query object and url identifier. Add console log.

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,7 +1,16 @@
-// our-domain.com/news/something-important
+import { useRouter } from "next/router";
+
+// our-domain.com/news/dynamic page
 
 function DetailsPage() {
+
+  const router = useRouter();
+
+  router.query.newsId
+  console.log(router.query.newsId);
+
   return <h1>The Details Page!</h1>;
+
 }
 
 export default DetailsPage;
